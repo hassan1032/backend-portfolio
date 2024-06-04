d8cb8bfc991fccd4fed0555203470dfa065de4cc
hassan1032 <khanhassan1032@gmail.com>
2024-06-04 13:44:37 +0530

    fgdfg

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-
-
-import express from "express"
+import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
 
 const app = express();
 
 config({ path: "./config/config.env" });
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 export default app;
