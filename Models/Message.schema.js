c661cc2e0eeb89e0823e31dbada00f8ce9b74298
hassan1032 <khanhassan1032@gmail.com>
2024-06-06 10:45:27 +0530

    Add new code

---
+++ Models/Message.schema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderName: {
+      type: String,
+      minLength: [2, "Name Musrt be at least 2 characters!"],
+    },
+    message: {
+      type: String,
+      minLength: [2, "Message must conatin at least 2 characters!"],
+    },
+    subject: {
+      type: String,
+      minLength: [2, "Subject must conatin at least 2 characters!"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = mongoose.model("Message",messageSchema)
