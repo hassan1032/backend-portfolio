590025dce16ad5e0e4ca8e0c0072ff3459273c08
hassan1032 <khanhassan1032@gmail.com>
2024-06-10 15:11:42 +0530

    Add user Api

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register, logout } from "../Controller/userController.js";
+import { login, register, logout, getUser } from "../Controller/userController.js";
 import  {isAuthenticated }from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -7,5 +7,5 @@ const router = express.Router();
 router.post("/create", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
-    
+router.get("/me",isAuthenticated,getUser)    
 export default router;
