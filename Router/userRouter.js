e0bb1809e0f10c0d83738b70e577b960f609d5a1
hassan1032 <khanhassan1032@gmail.com>
2024-06-24 15:32:48 +0530

    Changes code

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -8,7 +8,7 @@ router.post("/create", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/me",isAuthenticated,getUser)    
-router.put("/update-profile/:id", isAuthenticated, updateProfile);
+router.put("/update-profile", isAuthenticated, updateProfile);
 router.put("/update-password/:id", isAuthenticated, updatePassword);
 router.get("/me-get-portfolio",getUserPortfolio)
 router.post("/forgot-password",forgotPassword)
