d4af7f5d5bfbe1ec8f24c11944e6bc4c76d9e61c
hassan1032 <khanhassan1032@gmail.com>
2024-07-12 16:06:32 +0530

    add new code

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,7 +2,7 @@ export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   res
     .status(statusCode)
-    .cookie("token", token, {
+    .cookie("token1", token, {
       expire: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
