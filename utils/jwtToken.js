538e82a87ba81fc300ecbe5a554d374d1592b78d
hassan1032 <khanhassan1032@gmail.com>
2024-07-22 11:05:28 +0530

    Changes files

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,17 +1,29 @@
+// export const generateToken = (user, message, statusCode, res) => {
+//   const token = user.generateJsonWebToken();
+//   res
+//     .status(statusCode)
+//     .cookie("token1", token, {
+//       expire: new Date(
+//         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+//       ),
+//       httpOnly: true,
+//     })
+//     .json({
+//       success: true,
+//       message,
+//       user,
+//       token,
+//     });
+// };
+
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   res
     .status(statusCode)
-    .cookie("token1", token, {
-      expire: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-    })
     .json({
       success: true,
       message,
       user,
-      token,
+      token, // Include the token in the response body
     });
 };
