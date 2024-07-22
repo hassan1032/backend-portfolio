4e37772b4ec8a47e6cecc5c6e01066020a23e194
hassan1032 <khanhassan1032@gmail.com>
2024-07-22 11:32:37 +0530

    changes code

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,33 +1,17 @@
-// export const generateToken = (user, message, statusCode, res) => {
-//   const token = user.generateJsonWebToken();
-//   res
-//     .status(statusCode)
-//     .cookie("token1", token, {
-//       expire: new Date(
-//         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-//       ),
-//       httpOnly: true,
-//     })
-//     .json({
-//       success: true,
-//       message,
-//       user,
-//       token,
-//     });
-// };
-
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
-  const options = {
-    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Ensure secure cookie in production
-    sameSite: 'Strict',
-  };
-
-  res.status(statusCode).cookie('token', token, options).json({
-    success: true,
-    message,
-    user,
-  });
+  res
+    .status(statusCode)
+    .cookie("token1", token, {
+      expire: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message,
+      user,
+      token,
+    });
 };
