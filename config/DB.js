5efa05c72c4a99108c67c5355922e3a4ef98f0c1
Hassan khan <126570667+hassan1032@users.noreply.github.com>
2024-10-29 18:20:10 +0530

    Update DB.js

--- config/DB.js
+++ config/DB.js
@@ -1,18 +1,1 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const connectDB = async () => {
-  mongoose.connect(process.env.MONGO_URL,{
-    dbName: "Portfolio"
-  }).then(()=>{
-    console.log("MongoDB Connected")
-  }).catch((err)=>{
-    console.log(`MongoDB Connection Failed${err}`)
-  })
-  
-}
-
-export default connectDB;
 
